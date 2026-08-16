9e3993c5450433aa6b83d7ee1fa9fb386ef46018
hebaayase <hebaheshamayase@gmail.com>
2026-08-16 16:03:51 +0300

    Edit Folders

--- Sehatak.Application/DTOs/StaffLogIn/StaffLoginResponseDto.cs
+++ Sehatak.Application/DTOs/StaffLogIn/StaffLoginResponseDto.cs
@@ -9,7 +9,6 @@ namespace Sehatak.Application.DTOs.StaffLogIn
     public class StaffLoginResponseDto
     {
         public string AccessToken { get; set; } = string.Empty;
-        public string role { get; set; }
         public string RefreshToken { get; set; } = string.Empty;
     }
 }
